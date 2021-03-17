d470b7c9b0725bbe2d48c597da2a81c7f3409346
vipin.tripathi <vipin.tripathi@fosteringlinux.com>
2021-03-17 21:41:23 +0530

    Initial commit

---
+++ src/main/java/com/redhat/simple/calculator/BasicCalculator.java
@@ -0,0 +1,14 @@
+package com.redhat.simple.calculator;
+
+import java.util.Random;
+
+public final class BasicCalculator extends Calculator {
+
+
+    public int random() {
+        Random r = new Random();
+
+        return r.nextInt();
+    }
+}
+
